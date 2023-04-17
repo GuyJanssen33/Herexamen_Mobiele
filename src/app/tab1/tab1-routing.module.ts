718ca611cf67a088d889cbb9ded083d69baecd3b
GuyJanssen33 <r0237357@student.thomasmore.be>
2023-04-17 22:44:08 +0200

    eersteupdate

--- src/app/tab1/tab1-routing.module.ts
+++ src/app/tab1/tab1-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Tab1Page } from './tab1.page';
+import {Tab1PageModule} from "./tab1.module";
 
 const routes: Routes = [
   {
     path: '',
     component: Tab1Page,
+  },
+  {
+    path: 'Tab2Page/: id',
+    loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
   }
 ];
 
