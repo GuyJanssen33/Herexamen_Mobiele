b8240108acfd56255b2e84eb285fff9c618f2b8e
GuyJanssen33 <r0237357@student.thomasmore.be>
2023-08-06 15:13:04 +0200

    foutmelding na invoegen login component

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {PhoneVerificationComponent} from './phone-verification/phone-verification.component';
+import {Capacitor} from '@capacitor/core';
+import {AuthService} from '../services/auth.service';
+import {ModalController} from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  isNative = Capacitor.isNativePlatform();
+
+  constructor(public authService: AuthService, private modalController: ModalController) { }
+
+  async showPhoneVerification(): Promise<void> {
+    const modal = await this.modalController.create({
+      component: PhoneVerificationComponent
+    });
+    return await modal.present();
+  }
+
+  ngOnInit() {
+  }
+}
